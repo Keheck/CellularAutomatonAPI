8f7594986705efd3469a1af8e6755cc982ed7adb
Keheck <keheck@web.de>
2019-07-16 20:07:44 +0200

    Bug fix
    It would cycle into an invalid state before jumping back to 0

--- src/io/github/keheck/Cell.java
+++ src/io/github/keheck/Cell.java
@@ -71,7 +71,7 @@ public class Cell
 
     public final void cycleState()
     {
-        if(getState() >= getStates())
+        if(getState() > getStates())
             setState(0);
         else
             setState(getState()+1);
